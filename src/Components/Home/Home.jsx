40bb2b520c779bde413a90a32e29c48c1ff39a29
Bhoomika Pal <71694381+BhoomikaPal@users.noreply.github.com>
2024-01-12 16:34:28 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import './home.css'
 import video from '../Assets/video.mp4'
 import {GrLocation} from 'react-icons/gr'
+import {HiFilter} from 'react-icons/hi'
+
+
+
 const Home = () => {
+// creating a react hook to add a scroll animation
+
+
   return (
     <section className='home'>
       <div className="overlay"></div>
@@ -11,29 +18,39 @@ const Home = () => {
       <div className="homeContent container">
         <div className="textDiv">
 
-          <span className="smallText">
-            Our packages
-          </span>
-
-          <h1 className="homeTitle">
+          <h1 data-aos="fade-up"
+          className="homeTitle">
             Best Performer
           </h1>
 
         </div>
-        <div className="cardDiv grid"></div>
+        <div data-aos="fade-up" className="cardDiv grid">
         <div className="plantLocationInput"></div>
         <label htmlFor="city">Select the plant location</label>
         <div className="input flex">
           <input type="text" placeholder='Enter name here...'/>
           <GrLocation className="icon"/>
         </div>
+
         <div className="datInput"></div>
         <label htmlFor="date">Search your date:</label>
         <div className="input flex">
           <input type="date" placeholder='Enter name here...'/>
-          <GrLocation className="icon"/>
         </div>
-        
+        <div className="searchOptions flex">
+          <HiFilter className="icon"/>
+          <span>MORE PERFORMER</span>
+        </div>
+        </div>
+
+        <div data-aos="fade-up" className="homeFooterIcons flex">
+          <div className="rightIcons">
+
+          </div>
+          <div className="leftIcons">
+
+          </div>
+        </div>
       </div>
     </section>
   )
