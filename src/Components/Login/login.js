8f94171e4783a5ddb07293361780c6dc4251b76c
Shivam Kumar <skshivam252@gmail.com>
2024-02-27 22:30:20 +0530

    change

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -2,9 +2,20 @@ import './login.css';
 import profile from "../Assets/a.png";
 import email from "../Assets/email.jpg";
 import pass from "../Assets/pass.png";
-
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your login logic here (e.g., send data to backend API)
+    console.log('Email:', email);
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
   return (
     <div className="main">
      <div className="sub-main">
@@ -16,14 +27,30 @@ function Login() {
          </div>
          <div>
            <h1>Login Page</h1>
-           <div>
-             <img src={email} alt="email" className="email"/>
-             <input type="text" placeholder="user name" className="name"/>
-           </div>
-           <div className="second-input">
-             <img src={pass} alt="pass" className="email"/>
-             <input type="password" placeholder="user name" className="name"/>
-           </div>
+           <div className="input-group">
+            <i className="fas fa-user" />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <i className="fas fa-lock" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
           <div className="login-button">
           <button>Login</button>
           </div>
